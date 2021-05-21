7fe086c0f38fb3ed1102888fe1152fd9e728f5e9
Wonfa <willusetheforce@gmail.com>
2021-05-21 13:35:00 +0100

    End Menus

--- BTTF Game Jam/Assets/Scripts/Game.cs
+++ BTTF Game Jam/Assets/Scripts/Game.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Game : MonoBehaviour {
     public static Player Player { get; private set; }
+    public static Game Instance { get; private set; }
 
     private void Awake() {
         Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        Instance = this;
     }
+
+    [SerializeField]
+    private GameObject endScreenMenuFailed;
+    [SerializeField]
+    private GameObject endScreenMenuSuccess;
 
     public static void DeathEnding() {
         Player.Locked = true;
-        Player.SendMessage("Death Ending");
+        Instance.endScreenMenuFailed.SetActive(true);
     }
     public static void CompletedEnding() {
         Player.Locked = true;
-        Player.SendMessage("Completed Ending");
+        Instance.endScreenMenuSuccess.SetActive(true);
     }
 }
